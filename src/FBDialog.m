b52e7d73861e6b4375d401a9b2f77213b789357a
Stéphane Peter <megastep@megastep.org>
2011-06-07 01:29:05 -0700

    Updates to please Xcode 4.2 and its new LLVM

--- src/FBDialog.m
+++ src/FBDialog.m
@@ -37,7 +37,7 @@ static CGFloat kBorderWidth = 10;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-BOOL FBIsDeviceIPad() {
+static BOOL FBIsDeviceIPad() {
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 30200
   if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
     return YES;
@@ -135,7 +135,7 @@ BOOL FBIsDeviceIPad() {
   CGColorSpaceRelease(space);
 }
 
-- (BOOL)shouldRotateToOrientation:(UIDeviceOrientation)orientation {
+- (BOOL)shouldRotateToOrientation:(UIInterfaceOrientation)orientation {
   if (orientation == _orientation) {
     return NO;
   } else {
@@ -296,7 +296,7 @@ BOOL FBIsDeviceIPad() {
   if ((self = [super initWithFrame:CGRectZero]) != nil) {
     _delegate = nil;
     _loadingURL = nil;
-    _orientation = UIDeviceOrientationUnknown;
+    _orientation = (UIInterfaceOrientation) UIDeviceOrientationUnknown;
     _showingKeyboard = NO;
 
     self.backgroundColor = [UIColor clearColor];
@@ -454,7 +454,7 @@ BOOL FBIsDeviceIPad() {
 // UIDeviceOrientationDidChangeNotification
 
 - (void)deviceOrientationDidChange:(void*)object {
-  UIDeviceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+  UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
   if (!_showingKeyboard && [self shouldRotateToOrientation:orientation]) {
     [self updateWebOrientation];
 
